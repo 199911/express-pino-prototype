9fb14946b6c68b76f9095b62bb3482e70cd25b49
KU Lok Sun <sundaykuloksun@gmail.com>
2018-11-21 15:28:58 +0800

    Set up request id generation in request logger

--- server/middlewares/RequestLoggerMiddlewareFactory.js
+++ server/middlewares/RequestLoggerMiddlewareFactory.js
@@ -1,8 +1,12 @@
 const pinoHttp = require('pino-http');
+const uuidv4 = require('uuid/v4');
 
 const requestLoggerMiddlewareFactory = ({ logger }) => {
   return pinoHttp({
-    logger
+    logger,
+    // Have default ID which is increasing number per process, better use UUID
+    // to avoid collection in server cluster
+    genReqId: () => uuidv4(),
   });
 }
 
